4a33c9096165d2223101e2897dbdec2d8651924f
Belal <bb5503344@gmail.com>
2024-11-27 20:08:47 +0200

    oo

--- controllers/cart.contorller.js
+++ controllers/cart.contorller.js
@@ -4,18 +4,18 @@ const httpStatusText = require('../utils/httpStatusText');
 const appError = require('../utils/appError');
 
 const getCart = asyncWrapper(async (req, res) => {
-  const cart = await Cart.findOne({ userId: req.currentUser.id });
+  const cart = await Cart.findOne({ userId: req.currentUser._id });
   res.json({ status: httpStatusText.SUCCESS, data: { cart } });
 });
 
 const addToCart = asyncWrapper(async (req, res, next) => {
   const { productId, quantity, price } = req.body;
   
-  let cart = await Cart.findOne({ userId: req.currentUser.id });
+  let cart = await Cart.findOne({ userId: req.currentUser._id });
   
   if (!cart) {
     cart = new Cart({
-      userId: req.currentUser.id,
+      userId: req.currentUser._id,
       items: [{ productId, quantity, price }],
       totalAmount: quantity * price
     });
@@ -30,4 +30,5 @@ const addToCart = asyncWrapper(async (req, res, next) => {
 
 module.exports = {
     addToCart,
+    getCart
   };
--- routes/cart.route.js
+++ routes/cart.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middlewares/verifyToken');
+const CartController = require('../controllers/cart.contorller');
 
 router.use(verifyToken); // Protect all cart routes
 
-router.get('/', getCart);
-router.post('/add', addToCart);
-router.put('/update/:itemId', updateCartItem);
-router.delete('/remove/:itemId', removeFromCart);
+router.get('/', CartController.getCart);
+router.post('/add', CartController.addToCart);
+// router.put('/update/:itemId', updateCartItem);
+// router.delete('/remove/:itemId', removeFromCart);
 
 module.exports = router;
