daca824e90ac2a31b806b072b2a2c246ea3651cd
Belal <bb5503344@gmail.com>
2024-12-17 03:35:21 +0200

    oo

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose.connect(url).then(() => console.log("connected to database"));
 const app = express();
 
 app.use(cors({
-  origin: "https://royal-tex.surge.sh/" 
+  origin: "https://royal-tex.surge.sh" 
 }));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
